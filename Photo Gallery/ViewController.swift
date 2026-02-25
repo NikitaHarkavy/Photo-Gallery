79dbe98fcb13098a034dbffda8492c6a8340a60d
Nikita Harkavy <nikita.harkavy@appshero.io>
2026-02-25 05:51:35 +0300

    [ADDED] Initialized Xcode project

---
+++ Photo Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo Gallery
+//
+//  Created by Никита Горьковой on 18.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
